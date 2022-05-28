1a7f037a08a9ed4f023b170d0f149bb242134a84
gladyswambura <gladyswahito7@gmail.com>
2022-05-28 13:50:10 +0300

    fix:typo, category_name

--- gallery/models.py
+++ gallery/models.py
@@ -14,17 +14,18 @@ class Image(models.Model):
         return self.image_name
 
     @classmethod
-    def search_by_category(cls,search_term):
-        search_result = cls.objects.filter(image_category__cat_name__icontains=search_term)
+    def search_by_category(cls,search_iterm):
+        search_result = cls.objects.filter(image_category__cat_name__icontains=search_iterm)
         return search_result
 
     @classmethod
-    def get_image_by_id(cls,incoming_id):
-        image_result = cls.objects.get(id=incoming_id)
+    def get_image_by_id(cls,new_id):
+        image_result = cls.objects.get(id=new_id)
         return image_result
 
     def save_image(self):
         self.save()
+
     def delete_image(self):
         self.delete()
 
@@ -33,13 +34,13 @@ class Image(models.Model):
     def retrieve_all(cls):
         all_objects = Image.objects.all()
         for item in all_objects:
-            return item;
+            return item
 
 
     @classmethod
     def update_image(cls,current_value,new_value):
-        fetched_object = Image.objects.filter(image_name=current_value).update(image_name=new_value)
-        return fetched_object
+        updated_object = Image.objects.filter(image_name=current_value).update(image_name=new_value)
+        return updated_object
 
     @classmethod
     def filter_by_location(cls,location):
@@ -51,7 +52,7 @@ class Category(models.Model):
     category_name = models.CharField(max_length=255)
 
     def __str__(self):
-        return self.cat_name
+        return self.category_name
 
 
     def save_category(self):
