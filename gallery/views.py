e273be2b7051e4a15864d546410cbc40df809736
gladyswambura <gladyswahito7@gmail.com>
2022-05-28 13:28:00 +0300

    updates the views to base.html

--- gallery/views.py
+++ gallery/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
 
 # Create your views here.
+def index(request):
+    return render(request, 'base.html')
+
+def search_results(request):
+    return render(request, 'search.html')
+
+def category(request):
+    return render(request, 'base.html')
+
+def location(request):
+    return render(request, 'base.html')
+
+
+
+
+
+
