f3561cf8d13bdf861b31f652c797aa4f5cbf6b84
gladyswambura <gladyswahito7@gmail.com>
2022-05-29 21:56:04 +0300

    adds all models to be displayed in the landing page

--- gallery/views.py
+++ gallery/views.py
@@ -28,6 +28,18 @@ def search_results(request):
         message = "You haven't searched for any term"
         return render(request, 'search.html',{"message":message})
     
+def search_location(request):
+    if 'category' in request.GET and request.GET["category"]:
+        search_term = request.GET.get("category")
+        searched_category = Category.search_by_category(search_term)
+        message = f"{search_term}"
+
+        return render(request, 'search.html',{"message":message,"categories":searched_category})
+
+    else:
+        message = "You haven't searched for any term"
+        return render(request, 'search.html',{"message":message})
+    
 
 def category(request):
     return render(request, 'base.html')
