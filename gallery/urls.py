a1466764dcfbfd71f2cf0ec37f79c92792fbd45c
gladyswambura <gladyswahito7@gmail.com>
2022-05-30 00:55:56 +0300

    fix:fixes the url path bug

--- gallery/urls.py
+++ gallery/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('about/', views.about, name='about'),
     path('search/', views.search_results, name='search'),      
-    path('', views.category, name='category'),
-    path('', views.location, name='location'),
+    path('category/<str:category>/', views.category, name='category'),
+    path('location/<str:location>/', views.location, name='location'),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
